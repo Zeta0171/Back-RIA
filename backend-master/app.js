6010c4ebafd92f9c679f5094a5d471be8a34f3b8
Zeta0171 <santinozoccolini7@gmail.com>
2024-06-27 18:27:41 -0300

    cors solucionado

--- backend-master/app.js
+++ backend-master/app.js
@@ -8,19 +8,12 @@ const insumoRoutes = require('./routes/insumos');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger_output.json');
 
-const corsOptions = {
-  origin: 'http://localhost:4200', // Origen de tu aplicación Angular
-  optionsSuccessStatus: 200 // Algunos navegadores (Chrome) necesitan esta configuración
-};
-
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
-app.use(cors(corsOptions));
-
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/hospitales', hospitalesRoutes);
 app.use('/usuarios', usuariosRoutes);
